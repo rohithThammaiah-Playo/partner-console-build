0c28a9520449e732f486e71e2796e5b71e437b34
rohithThammaiah-Playo <94181649+rohithThammaiah-Playo@users.noreply.github.com>
2024-10-17 14:24:43 +0530

    Added Build files

---
+++ precache-manifest.d249ea7d72801f78d270c76a62d54183.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968cd9bd23837027eda4693a402a9e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e00af8e9e37b48a5af",
+    "url": "/static/css/2.c6d67f6a.chunk.css"
+  },
+  {
+    "revision": "18ba0b5671ba39f82bbb",
+    "url": "/static/css/main.0595247d.chunk.css"
+  },
+  {
+    "revision": "53e00af8e9e37b48a5af",
+    "url": "/static/js/2.f3bd52af.chunk.js"
+  },
+  {
+    "revision": "140c4eb03722cc6ead5fae585334f4c2",
+    "url": "/static/js/2.f3bd52af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ba0b5671ba39f82bbb",
+    "url": "/static/js/main.0894ed27.chunk.js"
+  },
+  {
+    "revision": "bde6792faca3fc4837f2",
+    "url": "/static/js/runtime-main.f77d042a.js"
+  },
+  {
+    "revision": "f3e2a23e555c1deec2822741a7846730",
+    "url": "/static/media/arrow.f3e2a23e.svg"
+  },
+  {
+    "revision": "2b6ef6f4bb0c21bcbaee6528c1b682c7",
+    "url": "/static/media/playo-logo.2b6ef6f4.svg"
+  }
+]);
